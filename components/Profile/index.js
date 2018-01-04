da77b818b38e7912aaa4e166449ba4eb08778ffa
Paul Fitzgerald <paulfitz99@gmail.com>
2018-01-04 22:51:07 +0000

    add bio box

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableHighlight, FlatList, Image } from 'react-native';
+import { View, Text, TouchableWithoutFeedback, FlatList, Image, TextInput } from 'react-native';
 import styled from 'styled-components/native';
 import ImagePicker from 'react-native-image-picker';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import firebase from '../../client';
 
 class Profile extends Component {
 
 	state = {
-		images: []
+		images: [],
+		bio: ''
 	}
 
 	componentDidMount() {
@@ -24,7 +26,9 @@ class Profile extends Component {
 				images.push(data);
 			});
 
-			console.log(images)
+			while(images.length < 6) {
+				images.push('');
+			}
 
 			this.setState({
 				images
@@ -44,11 +48,15 @@ class Profile extends Component {
 			image
 		});
 
-		this.setState({
-				images: [...this.state.images, image]
-		});
+		const images = [...this.state.images.filter(img => img !== '' ), image];
 
-		this.forceUpdate();
+		while(images.length < 6) {
+			images.push('');
+		}
+
+		this.setState({
+			images
+		});
 
 	}
 
@@ -63,19 +71,17 @@ class Profile extends Component {
 
 		ImagePicker.showImagePicker(options, (response) => {
 
-			console.log('Response = ', response);
-
-			if (response.error) {
+			if(response.didCancel) {
+				console.log('cancelled');
+			}
+			else if (response.error) {
 				console.log('ImagePicker Error: ', response.error);
 			}
 			else if (response.customButton) {
 				console.log('User tapped custom button: ', response.customButton);
 			}
 			else {
-				let source = { uri: response.uri };
 				this.addImageToUser(response.uri);
-				console.log(source);
-				// source is the source of the image
 			}
 		});
 	}
@@ -83,37 +89,92 @@ class Profile extends Component {
 	render() {
 
 		return (
-			<View>
-
-				<TouchableHighlight onPress={this.imageTest}>
-					<Text>IMAGES</Text>
-				</TouchableHighlight>
+			<ProfileContainer>
 
 				<ImagesContainer>
 				{
-					this.state.images.map(image => {
-						return (
-							<UsersImage key={ image } source={{ uri: image }} />
+					this.state.images.map((image, i) => {
+
+						return image === '' ? (
+							<TouchableWithoutFeedback
+								key={ i }
+								onPress={this.imageTest}
+							>
+								<ImageCol>
+									<EmptyImage>
+										<Icon name='plus-circle' size={25} color="#000" />
+									</EmptyImage>
+								</ImageCol>
+							</TouchableWithoutFeedback>
+						):
+
+					 (
+							<ImageCol key={ i }>
+								<UsersImage source={{ uri: image }} />
+							</ImageCol>
 						)
 					})
 				}
 				</ImagesContainer>
-			</View>
+
+
+				<BioContainer>
+					<BioInput
+	        	onChangeText={(bio) => this.setState({bio})}
+	        	value={this.state.bio}
+						placeholder='Bio...'
+						multiline
+					/>
+				</BioContainer>
+
+			</ProfileContainer>
 		);
 	}
 }
 
+const ProfileContainer = styled.View`
+	margin-top: 10px;
+`;
+
+const BioContainer = styled.View`
+	padding: 10px;
+	border-width: 1px;
+	border-color: #ccc;
+	border-radius: 4px;
+	margin: 15px;
+`;
+
+const BioInput = styled.TextInput`
+	height: 100px;
+`;
 
 const UsersImage = styled.Image`
-	height: 70px;
-	width: 70px;
-	border-radius: 35px;
+	height: 80px;
+	width: 80px;
+	border-radius: 40px;
+	background-color: #ccc;
+`;
+
+const EmptyImage = styled.View`
+	height: 80px;
+	width: 80px;
+	border-radius: 40px;
+	background-color: #ccc;
+	align-items: center;
+	justify-content: center;
 `;
 
 const ImagesContainer = styled.View`
 	display: flex;
 	flex-direction: row;
+	flex-wrap: wrap;
 	justify-content: space-around;
+`;
+
+const ImageCol = styled.View`
+	flex-basis: 33.333%;
+	align-items: center;
+	margin-bottom: 10px;
 `;
 
 export default Profile;
